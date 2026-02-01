b6ca08cf8fdbde0ffd6e426a3a12b65fa0d7a70b
artem1214 <61391756+artemanoh@users.noreply.github.com>
2026-02-01 16:44:25 +0200

    make hm

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useState } from "react";
+import { useCallback, useState } from "react";
 import "./App.css";
 import ImageGallery from "./components/ImageGallery";
 import Modal from "./components/Modal";
